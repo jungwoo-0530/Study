2c1a59205d46f81a1b42d5ab94a58abb578866ca
jungwoo-0530 <am7227@naver.com>
2021-09-26 05:43:59 +0900

    study

---
+++ ResourceLoader/src/test/java/spring/jungwoo/resourceloader/ResourceLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jungwoo.resourceloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
