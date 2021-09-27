0e5b38537ecaea5722fc9b4cc0e46ed7cc4ebb18
jungwoo-0530 <am7227@naver.com>
2021-09-28 02:49:26 +0900

    update

---
+++ propertyEditor/src/test/java/spring/jungwoo/propertyeditor/PropertyEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jungwoo.propertyeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
