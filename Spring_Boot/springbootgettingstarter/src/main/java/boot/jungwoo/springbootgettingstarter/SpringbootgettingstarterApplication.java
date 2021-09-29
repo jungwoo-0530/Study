9c34b9962e4ea1b29fc660d47c882f3fa527aff9
jungwoo-0530 <am7227@naver.com>
2021-09-30 03:55:14 +0900

    boot

--- Spring_Boot/springbootgettingstarter/src/main/java/boot/jungwoo/springbootgettingstarter/SpringbootgettingstarterApplication.java
+++ Spring_Boot/springbootgettingstarter/src/main/java/boot/jungwoo/springbootgettingstarter/SpringbootgettingstarterApplication.java
@@ -19,9 +19,11 @@ public class SpringbootgettingstarterApplication {
 //        holoman.setHowLong(10);
 //        return holoman;
 //    }
-// 버전업 되면서 빈 오버라이딩이 허용되지 않는다. 그러므로 오버라이딩을 하고 싶다면
+// 버전업 되면서 빈 오버라이딩이 허용되지 않는다. 그러므로 오버라이딩(재정의)을 하고 싶다면
 // setting spring.main.allow-bean-definition-overriding=true
-//
+// starter에서 @ConditionalOnMissingBean을 설정했기에 이 빈이 등록된다.
+
+    // 굳이 빈을 등록해서 사용해야하나? Application.properties에 값들을 등록해서 사용.
 
 
 }
