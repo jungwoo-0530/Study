629adeca80fb6cba1c06aa901058b4619efb8d2d
jungwoo-0530 <am7227@naver.com>
2021-10-01 23:45:13 +0900

    update

---
+++ Spring_Boot/HTTPS/src/test/java/boot/jungwoo/https/HttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jungwoo.https;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
