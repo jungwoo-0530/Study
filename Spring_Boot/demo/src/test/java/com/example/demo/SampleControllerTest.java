4ac7636e62e32e589d54c4f660e088eec7092690
jungwoo-0530 <am7227@naver.com>
2021-10-16 13:02:36 +0900

    HtmlUnit

--- Spring_Boot/demo/src/test/java/com/example/demo/SampleControllerTest.java
+++ Spring_Boot/demo/src/test/java/com/example/demo/SampleControllerTest.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,21 +21,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(SampleController.class)
 class SampleControllerTest {
 
+
     @Autowired
-    MockMvc mockMvc;
+    WebClient webClient;
 
     @Test
-    public void hello() throws Exception {
-        // 요청 "/hello"
-        // 응답
-        // - 응답 모델 name : jungwoo
-        // - 뷰 이름 : hello
-        mockMvc.perform(get("/hello"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("hello"))
-                .andExpect(model().attribute("name", is("jungwoo")))
-                .andExpect(content().string(containsString("jungwoo")));
+    public void hello() throws IOException {
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("jungwoo");
     }
 
 }
